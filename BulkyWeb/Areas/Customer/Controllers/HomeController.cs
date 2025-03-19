1fd196b874c27f6e5a337459a05214a2970280af
LIKHITH JAKKA VENKATA SUBBA <jakkalikhith@gmail.com>
2025-03-19 09:48:06 +0530

    Details page

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,17 +9,24 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+           IEnumerable<Product> productList= _unitOfWork.Product.GetAll(includeproperties: "Category");
+            return View(productList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.GET(u=>u.Id== productId, includeproperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
